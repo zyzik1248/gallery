d99b7c3021fdeea61e62829dc385e00c31782538
Zuzanna Dudzinska <zuzannadudzinska96@gmail.com>
2022-03-12 17:31:54 +0100

    add animation

--- src/components/photos/Photos.tsx
+++ src/components/photos/Photos.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { PhotoData } from "./../../rest";
 import Photo from "./Photo";
-import './photos.less'
+import "./photos.less";
 
 interface Props {
   photoList: PhotoData[];
 }
 
 const Photos: React.FC<Props> = ({ photoList }) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    ref?.current?.classList?.remove("photos-animation");
+
+    setTimeout(() => ref?.current?.classList?.add("photos-animation"), 300);
+  }, [photoList]);
+
   return (
-    <div className="photos">
+    <div ref={ref} className="photos photos-animation">
       {photoList.map((el) => (
         <Photo key={el.id} url={el.url} />
       ))}
