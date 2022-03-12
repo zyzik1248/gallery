c22df165e01e626352b3c28586e41aae3a70f2d5
Zuzanna Dudzinska <zuzannadudzinska96@gmail.com>
2022-03-12 16:46:42 +0100

    add button next and prev

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,46 @@ import PrimaryButton from "./components/buttons/PrimaryButton";
 const App = () => {
   const [photoList, setPhotoList] = useState<PhotoData[] | undefined>([]);
   const [activePhotos, setActivePhotos] = useState<PhotoData[] | undefined>([]);
+  const [position, setPosition] = useState(0);
 
   useEffect(() => {
     const fetch = async () => {
       const photoList = await fetchPhotos();
       setPhotoList(photoList);
-      setActivePhotos(photoList?.splice(0, 3));
+      setActivePhotos(photoList?.slice().splice(0, 3));
     };
 
     fetch();
   }, []);
 
+  const next = () => {
+    const pos = position + 3;
+    const list = photoList?.slice();
+    setActivePhotos(list?.splice(pos, 3));
+    setPosition(pos);
+  };
+
+  const prev = () => {
+    const pos = position - 3;
+    const list = photoList?.slice();
+    setActivePhotos(list?.splice(pos, 3));
+    setPosition(pos);
+  };
+
   return (
     <div className="app">
       <Photos photoList={activePhotos || []} />
       <div className="buttons">
-        <PrimaryButton label="next" onClick={() => {}} />
-        <PrimaryButton label="previous" onClick={()=>{}}/>
+        <PrimaryButton
+          disabled={position === 0}
+          label="previous"
+          onClick={prev}
+        />
+        <PrimaryButton
+          label="next"
+          onClick={next}
+          disabled={photoList && position > photoList?.length-4}
+        />
       </div>
     </div>
   );
--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import './button.less'
+import "./button.less";
 
 interface Props {
   label: string;
   onClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  disabled?: boolean;
 }
 
-const PrimaryButton: React.FC<Props> = ({ label, onClick }) => {
+const PrimaryButton: React.FC<Props> = ({
+  label,
+  onClick,
+  disabled = false,
+}) => {
   return (
-    <button className="button__primary" onClick={onClick}>
+    <button disabled={disabled} className="button__primary" onClick={onClick}>
       {label}
     </button>
   );
